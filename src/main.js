bdbcf80a1fcf234a6c53f617e249cc72f897b2f5
tyler kostelak <tkostelak@gmail.com>
2018-03-25 15:03:34 -0700

    form adds ability for user to submit a malady and receive 10 doctors in Seattle best suited for that practice.

--- src/main.js
+++ src/main.js
@@ -2,19 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-// import '../.env';
 
 $(document).ready(function() {
 
-  $('#maladySubmit').click(function(event){
+
+  $("#maladySubmit").click(function(event){
     event.preventDefault();
 
-    $('#result').text('');
+    $("#result").text('');
 
     let malady = $('#malady').val();
 
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${malady}&location=wa-seattle&skip=0&limit=10&user_key=`).then(function(response){
-      console.log(response);
+      if(response.data.length === 0)
+      {
+        $("#doctorResult").text('Your search criteria did not return any doctors, please try again!')
+      }
+
+      for(let i = 0; i < response.data.length; i ++)
+      {
+        $("#doctorResult").append(`<h3>${response.data[i].practices[0].name}</h3>`);
+      }
     });
   });
 });
